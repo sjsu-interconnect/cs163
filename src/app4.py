cfeec101705afcd65171cf87b69e3836d58789cd
EddieNguyen2012 <tuong62642@gmail.com>
2024-10-28 16:44:24 -0700

    Fix app.py

--- src/app4.py
+++ src/app4.py
@@ -1,9 +1,13 @@
-from dash import Dash, html, dash_table, dcc, Input, Output, callback
-from dash.dependencies import Input,Output
+import dash
 
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.LUX])
+
+# app.layout = html.Div("Hello User")
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
 
 
 
