d56b39d1af11b41676750ae484c851796219b51f
genyajoe <genya.ishigaki@sjsu.edu>
2025-02-24 22:03:20 -0800

    update app4 with cloud storage

--- appengine/app4.py
+++ appengine/app4.py
@@ -3,6 +3,32 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
+from google.cloud import storage
+import os
+from io import StringIO
+
+
+# BLOB is an acronym for "Binary Large Object". It's a data type that stores binary data, such as images, videos, and audio.
+def get_csv_from_gcs(bucket_name, source_blob_name):
+    """Downloads a blob from the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The ID of your GCS object
+    # source_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    blob = bucket.blob(source_blob_name)
+    data = blob.download_as_text()
+    return pd.read_csv(StringIO(data))
+
+
 # Incorporate data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
@@ -14,13 +40,20 @@ app = Dash()
 # https://dash.plotly.com/deployment
 server = app.server
 
+# Cloud Storage demo
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+df2 = get_csv_from_gcs(BUCKET_NAME, 'customers-100-simple.csv')
+
 # App layout
 app.layout = html.Div([
     html.Div(children='My First App with Data, Graph, and Controls'),
     html.Hr(),
     dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=6),
-    dcc.Graph(figure={}, id='controls-and-graph')
+    dcc.Graph(figure={}, id='controls-and-graph'),
+    html.Hr(),
+    # this is a separate table for google cloud storage demo
+    dash_table.DataTable(data=df2.to_dict('records'), page_size=6) 
 ])
 
 # Add controls to build the interaction
@@ -31,6 +64,7 @@ app.layout = html.Div([
 def update_graph(col_chosen):
     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
     return fig
+
 
 # Run the app
 if __name__ == '__main__':
